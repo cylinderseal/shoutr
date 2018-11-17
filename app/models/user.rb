4d4efd8fe1e1f5f61a2a60ca93abfd24f6909808
Al Barleta <al.barleta@gmail.com>
2018-11-17 19:13:14 +0000

    Allow user to shout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :shouts, dependent: :destroy
   include Clearance::User
   
   validates :username, presence: true, uniqueness: true
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   constraints Clearance::Constraints::SignedIn.new do
     root 'dashboards#show'
   end
-  get 'homes/show'
   root 'homes#show'
+  resources :shouts, only: [:create]
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
 
