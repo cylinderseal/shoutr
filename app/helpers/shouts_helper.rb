213eec93565bb6ab5543fc2007434bf671e4e84a
Al Barleta <al.barleta@gmail.com>
2018-11-20 06:32:42 +0000

    Add autolink functionality

--- app/helpers/shouts_helper.rb
+++ app/helpers/shouts_helper.rb
@@ -1,17 +1,15 @@
 module ShoutsHelper
   
-  def avatar(user)
-    email_digest = Digest::MD5.hexdigest(user)
-    gravatar_url = "//www.gravatar.com/avatar/#{email_digest}"
-    image_tag gravatar_url
-  end
-  
   def like_button(shout)
     if current_user.liked?(shout)
       link_to 'Unlike', unlike_shout_path(shout), method: :delete
     else
       link_to 'Like', like_shout_path(shout), method: :post
     end
+  end
+  
+  def autolink(text)
+    text.gsub(/@\w+/) { |mention| link_to mention, user_path(mention[1..-1]) }.html_safe
   end
   
 end
