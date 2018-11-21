5f09886c4bc4521340407d01f49c1ac07b768e46
Al Barleta <al.barleta@gmail.com>
2018-11-21 08:37:51 +0000

    Remove unused code from shouts helper

--- app/helpers/shouts_helper.rb
+++ app/helpers/shouts_helper.rb
@@ -2,7 +2,6 @@ module ShoutsHelper
   
   def shout_form_for(content_type)
     form_for(Shout.new, url: content_type.new) do |f|
-      f.hidden_field(:content_type, value: content_type) +
       f.fields_for(:content) { |c| yield(c) } +
       f.submit("Shout!")
     end
